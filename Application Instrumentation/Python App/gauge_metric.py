e2860dec2b1721884870e11c85a39efc41cc88b3
abbescheriif <abbes.cherif@enis.tn>
2025-12-16 15:21:05 +0100

    adding an other gauge metric and change the sleep time

--- Application Instrumentation/Python App/gauge_metric.py
+++ Application Instrumentation/Python App/gauge_metric.py
@@ -3,16 +3,18 @@ from prometheus_client import start_http_server, Gauge
 import time
 
 REQUEST_IN_PROGRESS = Gauge('request_in_progress', "Number of live Request on Application")
+REQUEST_LAST_EXECUTED = Gauge('request_last_served', "Time the application was last served")
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         REQUEST_IN_PROGRESS.inc()
-        time.sleep(7)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First python Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to the first Python application.</center></h2></body></html>", "utf-8"))
         self.wfile.close
+        time.sleep(7)
+        REQUEST_LAST_EXECUTED.set(time.time())
         REQUEST_IN_PROGRESS.dec()
 
 if __name__ == "__main__":
